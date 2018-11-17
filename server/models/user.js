d11923b5a5a1c6ca7c59a315a624fd2841ed90e2
vipinpapnai <vipin.papnai@qandle.com>
2018-11-17 11:26:37 +0530

    user auth with jwt

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,54 @@
-var mongoose = require('mongoose');
-var User = mongoose.model('User',{
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+var UserSchema = new mongoose.Schema({
   email:{
     required:true,
     trim: true,
     type: String,
-    minlength:3
-  }
+    minlength:3,
+    unique:true,
+    validate:{
+      validator: validator.isEmail,
+      message : '{VALUE} is not a valid email'
+    }
+  },
+  password:{
+    type: String,
+    required:true,
+    minlength:6
+  },
+  tokens:[{
+    access:{
+      type: String,
+      required:true
+    },
+    token:{
+      type:String,
+      required:true
+    }
+  }]
 });
+
+UserSchema.methods.toJSON = function(){
+  var user = this;
+  var userObj = user.toObject();
+  return _.pick(userObj,['_id','email'])
+}
+
+UserSchema.methods.generateAuthToken = function(){
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id:user._id.toHexString(),access},'abc123').toString();
+
+  user.tokens.push({access,token});
+  return user.save().then(()=>{
+    return token;
+  })
+}
+
+var User = mongoose.model('User',UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -86,10 +86,22 @@ app.patch('/todos/:id',(req,res)=>{
   }).catch((e)=>{
      return res.status(404).send();
   })
-
-
-
 })
+
+app.post('/users',(req,res)=>{
+  var body = _.pick(req.body,['email','password']);
+  var user = new User(body);
+
+
+
+  user.save().then((user)=>{
+      return user.generateAuthToken();
+  }).then((token)=>{
+      res.header('x-auth',token).send(user);
+  }).catch((e)=>{
+    return res.status(400).send(e);
+  })
+});
 
 
 app.listen(port,()=>{
